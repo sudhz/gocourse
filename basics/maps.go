431bf7a2ef5c3bfbb4b1a686ee209fd960a6f274
Sudhanshu Makwana <shudhanshumakwana@gmail.com>
2026-05-06 13:27:23 +0530

    add maps practice

---
+++ basics/maps.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"maps"
+)
+
+func main() {
+	myMap := make(map[string]int)
+
+	fmt.Println(myMap)
+
+	myMap["key1"] = 9
+	myMap["code"] = 18
+
+	fmt.Println(myMap)
+
+	fmt.Println(myMap["key1"])
+	fmt.Println(myMap["key"])
+
+	myMap["code"] = 21
+	fmt.Println(myMap)
+
+	delete(myMap, "key1")
+
+	fmt.Println(myMap)
+
+	myMap["key1"] = 9
+	myMap["key2"] = 10
+	myMap["key3"] = 11
+
+	fmt.Println(myMap)
+
+	_, ok := myMap["key1"]
+	if ok {
+		fmt.Println("a value exists with key1")
+	} else {
+		fmt.Println("no value exists with key1")
+	}
+
+	myMap2 := map[string]int{"a": 1, "b": 2}
+	fmt.Println(myMap2)
+
+	myMap3 := map[string]int{"a": 1, "b": 2}
+
+	if maps.Equal(myMap3, myMap2) {
+		fmt.Println("myMap3 and myMap2 are equal")
+	}
+
+	for k, v := range myMap3 {
+		fmt.Printf("myMap3 key: %s, value: %d\n", k, v)
+	}
+
+	var myMap4 map[string]string
+
+	if myMap4 == nil {
+		fmt.Println("the map is initialised to nil value")
+	} else {
+		fmt.Println("the map is not initialised to nil value")
+	}
+
+	val := myMap4["key"]
+	fmt.Println("value at key:", val)
+
+	myMap4 = make(map[string]string)
+	myMap4["key"] = "value"
+	fmt.Println(myMap4)
+
+	fmt.Println("myMap length is:", len(myMap))
+
+	myMap5 := make(map[string]map[string]string)
+	myMap5["map1"] = myMap4
+	fmt.Println(myMap5)
+
+	clear(myMap)
+	fmt.Println(myMap)
+}
