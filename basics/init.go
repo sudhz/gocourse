68ef66431d934fd4c7ed55378eab8b6a37ff5b5c
Sudhanshu Makwana <shudhanshumakwana@gmail.com>
2026-05-17 12:50:23 +0530

    add exit practice

---
+++ basics/init.go
@@ -0,0 +1,19 @@
+package basics
+
+import "fmt"
+
+func init() {
+	fmt.Println("1. initialising package...")
+}
+
+func init() {
+	fmt.Println("2. initialising package...")
+}
+
+func init() {
+	fmt.Println("3. initialising package...")
+}
+
+func main() {
+	fmt.Println("inside the main function")
+}
